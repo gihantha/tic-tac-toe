c0e98d8a79b1245efff5e2d6a4c55c2bf9ab1a67
gihantha <gihantha01@gmail.com>
2024-09-03 08:19:01 +0530

    feat(ui): add buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import './styles.css';
 
-function App() {
+function Board() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <>
+    <div>
+      <button >1</button>
+      <button >2</button>
+      <button >3</button>
     </div>
+    <div>
+      <button >4</button>
+      <button >5</button>
+      <button >6</button>
+    </div>
+    <div>
+      <button >7</button>
+      <button >8</button>
+      <button >9</button>
+    </div>
+    </>
   );
 }
 
-export default App;
+export default Board;
