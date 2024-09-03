a024a59be9e427b62d441ed40ac44388a611db0c
gihantha <gihantha01@gmail.com>
2024-09-03 09:01:36 +0530

    feat:edit the Board component so that it declares a state variable

--- src/App.js
+++ src/App.js
@@ -2,35 +2,33 @@ import { useState } from "react";
 import "./App.css";
 import "./styles.css";
 
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue('X');
-  }
+function Square({value}) {
+  
 
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square">
       {value}
     </button>
   );
 }
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square values={squares[0]}/>
+        <Square values={squares[1]}/>
+        <Square values={squares[2]}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square values={squares[3]}/>
+        <Square values={squares[4]}/>
+        <Square values={squares[5]}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square values={squares[6]}/>
+        <Square values={squares[7]}/>
+        <Square values={squares[8]}/>
       </div>
     </>
   );
